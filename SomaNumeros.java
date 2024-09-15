041afedd2b38a0d76aec5133fe43d2b2d7c815f0
Luace83 <luara_figueiredo@edu.unifor.br>
2024-09-15 14:39:29 -0300

    SomaNumeros java

--- SomaNumeros.java
+++ SomaNumeros.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class SomaDeNumeros {
+ class SomaNumeros {
+  public static void main(String[] args) {
   Scanner prompt = new Scanner(System.in);
-  int n, num, soma = 0, i = 0;
-  public void SomaDeNumeros(){
+  int n, num = 0, soma = 0, i = 0;
+  
     System.out.print("Quantos numeros a serem somados: ");
-    int n = prompt.nextInt();
+     n = prompt.nextInt();
       while(i<n) {
         i = i + 1;
-        System.out.println("Entre com o "+ i + numero");
+        System.out.println("Entre com o numero");
 
         num = prompt.nextInt();
         soma = soma + num;
