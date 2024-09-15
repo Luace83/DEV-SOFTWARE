f5a7a980f6397655f8dd14235a11b8dabcd3ec83
Luace83 <luara_figueiredo@edu.unifor.br>
2024-09-15 14:21:33 -0300

    NumerosPrimos java

--- NumerosPrimos.java
+++ NumerosPrimos.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VerificaPrimo {
+ class NumerosPrimos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número: ");
